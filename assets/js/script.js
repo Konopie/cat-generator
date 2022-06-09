051275eefbdf46adbeecd8fd45bb272ef4fd156c
Cody Cooper <codycooper06@gmail.com>
2022-06-09 19:53:34 -0400

    cat image displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
+// fetch cat fact array
 fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
     response.json().then(function(data) {
         console.log(data);
     })
 });
 
+// fetch cat picture url
 var catImageUrl = fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
     response.json().then(function(data) {
-        console.log(data[0].url);
+        // set variable to fetched url
+        catImageUrl = data[0].url;
+        // display image after 
+        displayImage();
     })
 });
+
+// push custom image url into image handler
+function displayImage() {
+    // create img element
+    var img = document.createElement('img');
+    // set img url to custom url
+    img.src = catImageUrl;
+    // append img to page
+    document.getElementById('imageHandler').appendChild(img); 
+} 
+
+
+
 
 
