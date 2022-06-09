02931929cc537a0ec0bea0a47bd1746a80226039
Cody Cooper <codycooper06@gmail.com>
2022-06-09 19:23:00 -0400

    cat image api added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,10 @@ fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
     })
 });
 
+var catImageUrl = fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
+    response.json().then(function(data) {
+        console.log(data[0].url);
+    })
+});
 
 
