f7338bfd229a9080d01488d0ecfbbf54f4fc6e39
Cody Cooper <codycooper06@gmail.com>
2022-06-09 21:26:26 -0400

    new picture generated with every button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var picBtn = document.querySelector("#pic-generator");
 
 // fetch cat fact array
 fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
@@ -6,10 +7,13 @@ fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
     })
 });
 
+
+    
+
 // fetch cat picture url
-var catImageUrl = function() {
-    if (document.contains("img")) {
-    document.getElementById("imageHandler").removeChild(img)};
+function fetchUrl(){
+    if (document.getElementById("imageHandler").contains(document.querySelector(".catImage"))) {
+        document.getElementById("imageHandler").removeChild(document.querySelector(".catImage"))};
     fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
     response.json().then(function(data) {
         // set variable to fetched url
@@ -32,7 +36,7 @@ function displayImage() {
     document.getElementById('imageHandler').appendChild(img); 
 } 
 
-
+picBtn.addEventListener("click", fetchUrl);
 
 
 
