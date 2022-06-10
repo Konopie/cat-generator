be6dd2dbaebfc682a62ed561a4827e10473d228c
bethanyshoun <bethanyshoun@gmail.com>
2022-06-09 21:44:25 -0400

    set up functions for cat facts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,17 @@
 var catFacts = fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
     response.json().then(function(data) {
         console.log(data);
+        catFacts = data[0].text;
+        displayFact();
     })
 });
+
+//push cat fact into fact handler
+function displayFact() {
+    var factText = document.createElement('p');
+    factText.textContent = catFacts;
+    document.getElementById('factHandler').appendChild(factText);
+}
 
 // fetch cat picture url
 var catImageUrl = fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
