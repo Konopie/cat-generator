742fff535fbd58d3a53ee2329aa78b069d8938cf
Cody Cooper <codycooper06@gmail.com>
2022-06-09 20:43:53 -0400

    image appears on button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,25 @@ fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
 });
 
 // fetch cat picture url
-var catImageUrl = fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
+var catImageUrl = function() {
+    if (document.contains("img")) {
+    document.getElementById("imageHandler").removeChild(img)};
+    fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
     response.json().then(function(data) {
         // set variable to fetched url
         catImageUrl = data[0].url;
         // display image after 
         displayImage();
     })
-});
+})};
 
 // push custom image url into image handler
 function displayImage() {
+    
     // create img element
     var img = document.createElement('img');
+    // set img to css class
+    img.className = "catImage";
     // set img url to custom url
     img.src = catImageUrl;
     // append img to page
