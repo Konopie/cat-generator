536b6399dda10de1d7e1f19e9eca60cab4c17b95
mackgabe <mackenzieabe@gmail.com>
2022-06-11 09:14:14 -0400

    added star icon to favorite button as a placeholder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
-var
-
-=======
-// fetch cat fact array
->>>>>>> a7e6c2641e3a2f2e677f932fd90cab309e80a2ce
-fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-    })
-});
+//// fetch cat fact array
+//// a7e6c2641e3a2f2e677f932fd90cab309e80a2ce
+//fetch("https://cat-fact.herokuapp.com/facts").then(function(response) {
+   // response.json().then(function(data) {
+     //   console.log(data);
+  //  })
+//});
 
 // fetch cat picture url
+
 var catImageUrl = fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
     response.json().then(function(data) {
         // set variable to fetched url
