532c747ee7f1a512c407478798ee93bc2a47878a
bethanyshoun <bethanyshoun@gmail.com>
2022-06-12 22:59:53 -0400

    README Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,13 +32,12 @@ function displayFact() {
 
 
 // fetch cat picture url
-
-//var catImageUrl = "thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294"
 function fetchUrl(){
     if (document.getElementById("imageHandler").contains(document.querySelector(".catImage"))) {
         document.getElementById("imageHandler").removeChild(document.querySelector(".catImage"))};
     fetch("https://api.thecatapi.com/v1/images/search?api_key=0e2cc572-50b2-4f1f-b996-1ea6cc513294").then(function(response) {
     response.json().then(function(data) {
+        console.log(data)
         // set variable to fetched url
         catImageUrl = data[0].url;
         // display image after 
